4173895f8d82b31ca0f83d2bb5e1ca30fb71fb18
Aaimry <wheal28@gmail.com>
2022-03-05 17:34:10 +0600

    зарегистрировали модели в админке

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from webapp.models import Product, Review
 
-# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'category', 'description']
+    fields = ['title', 'category', 'description', 'picture']
+
+
+admin.site.register(Product, ProductAdmin)
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ['id', 'product', 'author', 'rate']
+    fields = ['id', 'product', 'author', 'review_text', 'rate', 'is_moderated']
+
+
+admin.site.register(Review, ReviewAdmin)
